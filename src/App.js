6df346cb08661e4a405ef788835aa5d5ae11cf90
Amardeep T <amardeept@annait.net>
2020-07-30 18:32:52 +0530

    all good

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
@@ -6,10 +7,12 @@ import ProfileForm from './components/form-components/profile';
 
 function App() {
   return (
-    <div className="container">
-      Hello React app
-      <ProfileForm/>
-    </div>
+    <Router>
+      <div className="container">
+        <Route exact path="/" component={ProfileForm} />
+      </div>
+    </Router>
+    
   );
 }
 
--- src/components/form-components/profile.js
+++ src/components/form-components/profile.js
@@ -1,33 +1,95 @@
 import  React, { useState }from 'react';
 
 const ProfileForm = () => {
+    const [profile, setProfile] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        dob: '',
+        gender: '',
+        location: '',
+        workes: [],
+        plocation: '',
+        industry: '',
+        range: ''
+    });
 
+    const addWork = (e) => {
+        e.preventDefault();
+        var p = { company: '', designation: '', from_date: '', to_date: '' };
+        setProfile({
+            ...profile,
+            workes : [...profile.workes, p]
+        });
+    }
+
+    const handleCompanyChange = (e, index) => {
+        let nameValue = e.target.name;
+        //console.log("name value is: ",nameValue);
+        //console.log("value is: ", e.target.value);
+        //console.log("index is: ", index);
+        profile.workes[index][nameValue] = e.target.value;
+
+        //console.log("profiles are : ", profile);
+
+        //now set profiles to state
+        setProfile({
+            ...profile,
+            workes : profile.workes
+        }); 
+    }
+
+    const onSubmitHandler =  (e) => {
+        e.preventDefault();
+
+        console.log("profile si: ", profile);
+        localStorage.setItem('profile', JSON.stringify(profile));
+    }
+    
+
+    const onChangeNormalParameters = (e) => {
+        setProfile({ ...profile, [e.target.name]: e.target.value });
+        console.log("profile si: ", profile);
+
+    }
+
+    const removeWork = (e, index) => {
+        e.preventDefault();
+
+        profile.workes.splice(index,1);
+
+        setProfile({
+            ...profile,
+            workes: profile.workes
+        }); 
+
+    }
 
     return (
         <div>
             <h2>Profile Form</h2>
-            <form >
+            <form onSubmit={onSubmitHandler}>
                 <div className="form-group">
-                    <label for="email">Name</label>
-                    <input type="name" className="form-control" placeholder="Enter name" id="email" />
+                    <label htmlFor="email">Name</label>
+                    <input type="name" value={profile.name} name="name" onChange={onChangeNormalParameters} className="form-control" placeholder="Enter name" id="name" />
                 </div>
                 <div className="form-group">
-                        <label for="email">Email address:</label>
-                        <input type="email" className="form-control" placeholder="Enter email" id="email"/>
+                        <label htmlFor="email">Email address:</label>
+                    <input type="email" name="email" value={profile.email} onChange={onChangeNormalParameters} className="form-control" placeholder="Enter email" id="email"/>
                 </div>
                 <div className="form-group">
-                    <label for="email">Mobile</label>
-                    <input type="number" className="form-control" placeholder="Enter mobile" id="mobile" />
+                    <label htmlFor="email">Mobile</label>
+                    <input type="number" name="phone" value={profile.phone} onChange={onChangeNormalParameters} className="form-control" placeholder="Enter mobile" id="mobile" />
                 </div>
                 
                 <div className="form-group">
-                    <label for="email">DOB</label>
-                    <input type="date" className="form-control" placeholder="Enter DOB" id="dob" />
+                    <label htmlFor="email">DOB</label>
+                    <input type="date" name="dob" value={profile.dob} onChange={onChangeNormalParameters} className="form-control" placeholder="Enter DOB" id="dob" />
                 </div>
 
                 <div className="form-group">
-                    <label for="email">select gender</label>
-                    <select className="form-control">
+                    <label htmlFor="email">select gender</label>
+                    <select name="gender" className="form-control" value={profile.gender} onChange={onChangeNormalParameters}>
                         <option >select gender</option>
                         <option value="Male">Male</option>
                         <option value="Female">Female</option>
@@ -35,33 +97,52 @@ const ProfileForm = () => {
                 </div>
 
                 <div className="form-group">
-                    <label for="email">Location</label>
-                    <input type="text" className="form-control" placeholder="Enter location" id="location" />
+                    <label htmlFor="email">Location</label>
+                    <input type="text" name="location" value={profile.location} onChange={onChangeNormalParameters} className="form-control" placeholder="Enter location" id="location" />
                 </div>
 
                 
 
                 <div className="company-details">
-                    <h3>Work Experience</h3>
-                    <div className="each-company">
-                        <h4>Company 1</h4>
-                        <div className="form-group">
-                            <label for="email">Company</label>
-                            <input type="text" className="form-control" placeholder="Enter company name" id="company" />
-                        </div>
-                        <div className="form-group">
-                            <label for="email">Designation</label>
-                            <input type="text" className="form-control" placeholder="Enter designation" id="designation" />
-                        </div>
-                        <div className="form-group">
-                            <label for="email">From date</label>
-                            <input type="date" className="form-control" placeholder="Enter date" id="fdate" />
-                        </div>
-                        <div className="form-group">
-                            <label for="email">To date</label>
-                            <input type="date" className="form-control" placeholder="Enter date" id="tdate" />
-                        </div>
-                    </div>
+                    <h3>Work Experience <button onClick={(e) => addWork(e)}  className="btn btn-info btn-sm">Add</button></h3>
+                    
+                    
+                        {
+                            profile.workes.map( ( workk, index ) => {
+                                return (
+                                    <div className="each-company" key={index}>
+                                        <h4>Company {index + 1}
+                                          <button onClick={(e) => removeWork(e, index)} className="btn btn-danger btn-sm">Remove</button>
+                                        </h4>
+                                        <div className="col-sm-6">
+                                            <div className="form-group">
+                                                <label htmlFor="email">Company</label>
+                                                <input type="text" name="company" value={workk.company} className="form-control" placeholder="Enter company name" id="company" onChange={e => handleCompanyChange(e, index)} />
+                                            </div>
+                                            <div className="form-group">
+                                                <label htmlFor="email">Designation</label>
+                                                <input type="text" name="designation" value={workk.designation} className="form-control" placeholder="Enter designation" id="designation" onChange={e => handleCompanyChange(e, index)} />
+                                            </div>
+                                        </div>
+                                        <div className="col-sm-6">
+                                            <div className="form-group">
+                                                <label htmlFor="email">Working from</label>
+                                                <input name="from_date" type="text" value={workk.from_date}   className="form-control" placeholder="Enter date" id="fdate" onChange={e => handleCompanyChange(e, index)} />
+                                            </div>
+                                            <div className="form-group">
+                                                <label htmlFor="email">Till </label>
+                                                <input name="to_date" value={workk.to_date} type="text"   className="form-control" placeholder="Enter date" id="tdate" onChange = {e => handleCompanyChange(e, index)} />
+                                            </div>
+                                        </div>   
+                                        
+                                    </div>
+                                    
+                                )
+                            })
+                        }
+                        
+                        
+                    
                     
 
                 </div>
@@ -69,23 +150,23 @@ const ProfileForm = () => {
                 <div className="company-preferences">
                     <h3>Preferences</h3>
                         <div className="form-group">
-                            <label for="email">Preferred Location</label>
-                            <input type="text" className="form-control" placeholder="Enter preferred location" id="plocation" />
+                            <label htmlFor="email">Preferred Location</label>
+                        <input type="text" name="plocation" value={profile.plocation} onChange={onChangeNormalParameters} className="form-control" placeholder="Enter preferred location" id="plocation" />
                         </div>
                         <div className="form-group">
-                            <label for="email">Industry</label>
-                            <input type="text" className="form-control" placeholder="Enter industry" id="industry" />
+                            <label htmlFor="email">Industry</label>
+                        <input type="text" name="industry" value={profile.industry} onChange={onChangeNormalParameters} className="form-control" placeholder="Enter industry" id="industry" />
                         </div>
                         <div className="form-group">
-                            <label for="email">Salary Range</label>
-                            <input type="text" className="form-control" placeholder="Enter salary range" id="srange" />
+                            <label htmlFor="email">Salary Range</label>
+                        <input type="text" name="range" value={profile.range} onChange={onChangeNormalParameters} className="form-control" placeholder="Enter salary range" id="srange" />
                         </div>
                       
                         
                 </div>
                 
                         
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button  type="submit" className="btn btn-primary">Submit</button>
             </form>
         </div>
     )
